0dad581d47fef1d2fb5b837cf48689656311a2b9
Bartosz  Makara <bartoszmakara@pc-140581.home>
2022-10-02 22:58:11 +0100

    Day 2 Done

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,11 @@
 import { SearchIcon, BellIcon } from "@heroicons/react/outline";
 import Link from "next/link";
 import { useEffect, useState } from "react";
+import useAuth from "../hooks/useAuth";
 
 function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
+  const { logout } = useAuth();
 
   useEffect(() => {
     const handleScroll = () => {
@@ -50,6 +52,7 @@ function Header() {
             src="https://rb.gy/g1pwyx"
             alt=""
             className="cursor-pointer rounded"
+            onClick={logout}
           />
         </Link>
       </div>
